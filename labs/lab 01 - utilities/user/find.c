f0b3eca90008a598cb1206439985d3d6635e6806
Yassine Zakhama <yzakhama@gmail.com>
2026-04-04 22:07:11 +0200

    lab 1: implement find

---
+++ labs/lab 01 - utilities/user/find.c
@@ -0,0 +1,72 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/fs.h"
+
+void find(char* dir, char* file);
+
+int main(int argc, char* argv[]) {
+  if (argc == 2) {
+    find(".", argv[1]);
+  } else if (argc == 3) {
+    find(argv[1], argv[2]);
+  } else {
+    fprintf(2, "Usage: find <dir> <filename>\n");
+    exit(1);
+  }
+
+  exit(0);
+}
+
+void find(char* dir, char* file) {
+  char buf[512], *p;
+  int fd;
+  struct dirent de;
+  struct stat st;
+
+  if ((fd = open(dir, 0)) < 0) {
+    fprintf(2, "cannot open %s\n", dir);
+    return;
+  }
+
+  if (fstat(fd, &st) < 0) {
+    fprintf(2, "cannot open %s\n", dir);
+    return;
+  }
+
+  if (st.type != T_DIR) {
+    fprintf(2, "%s is not a directory\n", dir);
+  }
+
+  if(strlen(dir) + 1 + DIRSIZ + 1 > sizeof buf){
+    printf("path too long\n");
+    return;
+  }
+  
+  strcpy(buf, dir);
+  p = buf+strlen(buf);
+  *p++ = '/';
+  
+  while(read(fd, &de, sizeof(de)) == sizeof(de)){
+    if(de.inum == 0 || strcmp(de.name, ".") == 0 || strcmp(de.name, "..") == 0) {
+      continue; // inum == 0 := empty slot
+    }
+    
+    memmove(p, de.name, DIRSIZ);
+    p[DIRSIZ] = 0;
+    
+    if(stat(buf, &st) < 0){
+      fprintf(2, "cannot stat %s\n", buf);
+      continue;
+    }
+
+    if (st.type == T_DIR) {
+      find(buf, file);
+      continue;
+    } else if (strcmp(de.name, file) == 0) {
+      printf("%s\n", buf);
+    }
+  }
+
+  close(fd);
+}
